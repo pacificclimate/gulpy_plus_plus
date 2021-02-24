743c856a40464ac8beebfb55ab59a32ecadd2f56
James Hiebert <james@hiebert.name>
2021-02-24 12:14:05 -0800

    Parametrize diagnostic mode and sample size

--- gulpy_plus_plus.py
+++ gulpy_plus_plus.py
@@ -14,14 +14,17 @@ from crmprtd.insert import insert
 logging.basicConfig(level="DEBUG")
 
 @contextmanager
-def transaction_to_rollback(connection_string):
+def transaction_to_rollback(connection_string, diagnostic=True):
     engine = create_engine(connection_string)
     connection = engine.connect()
     transaction = connection.begin()
     sesh = Session(bind=connection)
     yield sesh
     sesh.close
-    transaction.rollback()
+    if diagnostic:
+        transaction.rollback()
+    else:
+        transaction.commit()
     connection.close()
 
 
@@ -30,7 +33,15 @@ if __name__ == '__main__':
     parser.add_option("-c", "--connection-string", dest="connection_string",
                       help="database connection string",
                       default="postgresql://crmp@monsoon.pcic.uvic.ca/crmp"
-                      )
+    )
+    parser.add_option("-d", "--diagnostic", default=True,
+                      help="Diagnostic mode. Should insertions be rolled back?"
+    )
+    parser.add_option("-s", "--sample-size", dest="sample_size", default=100,
+                      help="Number of observations to pre-check for duplicates "
+                      "to decide insert strategy. Recommended to be < 10% of "
+                      "the number of observations")
+
     (options, filenames) = parser.parse_args()
 
     for fname in filenames:
@@ -39,6 +50,6 @@ if __name__ == '__main__':
             reader = csv.DictReader(csvfile, fieldnames)
             obs = [Obs(**row) for row in islice(reader, 1, None)]
 
-        with transaction_to_rollback(options.connection_string) as sesh:
-            results = insert(sesh, obs, 100)
+        with transaction_to_rollback(options.connection_string, options.diagnostic) as sesh:
+            results = insert(sesh, obs, options.sample_size)
             print(results)
