843ddf94f3d64be37cd0569b13c140c8a5ac6af5
James Hiebert <james@hiebert.name>
2021-02-24 12:14:31 -0800

    Reformat with black

--- gulpy_plus_plus.py
+++ gulpy_plus_plus.py
@@ -13,6 +13,7 @@ from crmprtd.insert import insert
 
 logging.basicConfig(level="DEBUG")
 
+
 @contextmanager
 def transaction_to_rollback(connection_string, diagnostic=True):
     engine = create_engine(connection_string)
@@ -28,19 +29,30 @@ def transaction_to_rollback(connection_string, diagnostic=True):
     connection.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = OptionParser()
-    parser.add_option("-c", "--connection-string", dest="connection_string",
-                      help="database connection string",
-                      default="postgresql://crmp@monsoon.pcic.uvic.ca/crmp"
+    parser.add_option(
+        "-c",
+        "--connection-string",
+        dest="connection_string",
+        help="database connection string",
+        default="postgresql://crmp@monsoon.pcic.uvic.ca/crmp",
     )
-    parser.add_option("-d", "--diagnostic", default=True,
-                      help="Diagnostic mode. Should insertions be rolled back?"
+    parser.add_option(
+        "-d",
+        "--diagnostic",
+        default=True,
+        help="Diagnostic mode. Should insertions be rolled back?",
     )
-    parser.add_option("-s", "--sample-size", dest="sample_size", default=100,
-                      help="Number of observations to pre-check for duplicates "
-                      "to decide insert strategy. Recommended to be < 10% of "
-                      "the number of observations")
+    parser.add_option(
+        "-s",
+        "--sample-size",
+        dest="sample_size",
+        default=100,
+        help="Number of observations to pre-check for duplicates "
+        "to decide insert strategy. Recommended to be < 10% of "
+        "the number of observations",
+    )
 
     (options, filenames) = parser.parse_args()
 
@@ -50,6 +62,8 @@ if __name__ == '__main__':
             reader = csv.DictReader(csvfile, fieldnames)
             obs = [Obs(**row) for row in islice(reader, 1, None)]
 
-        with transaction_to_rollback(options.connection_string, options.diagnostic) as sesh:
+        with transaction_to_rollback(
+            options.connection_string, options.diagnostic
+        ) as sesh:
             results = insert(sesh, obs, options.sample_size)
             print(results)
